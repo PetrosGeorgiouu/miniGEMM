b6e473049e3d3b9c5fd847fc374bb4504c643672
PetrosGeorgiouu <pag238@cornell.edu>
2026-04-03 04:28:57 -0400

    Redid bruteforce to collapse all functions into a single GEMM routine

--- bruteforce.cpp
+++ bruteforce.cpp
@@ -1,17 +1,40 @@
 #include "bruteforce.h"
 #include <iostream>
 
-void bruteForceMatMult(const float *A, const float *B, float *C, int n,
-                       int k, int m)
+void GEMM(const float *A, const float *B, float *C, float alpha, float beta,
+          int n, int k, int m)
 {
+  float AB[n][m];
+
   for (int i = 0; i < n; i++)
   {
     for (int j = 0; j < m; j++)
     {
       for (int l = 0; l < k; l++)
       {
-        C[i * m + j] += A[i * k + l] * B[l * m + j];
+        AB[i][j] += A[i * k + l] * B[l * m + j];
       }
+    }
+  }
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < m; j++)
+    {
+      AB[i][j] *= alpha;
+    }
+  }
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < m; j++)
+    {
+      C[i * m + j] *= beta;
+    }
+  }
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < m; j++)
+    {
+      C[i * m + j] += AB[i][j];
     }
   }
 }
--- bruteforce.h
+++ bruteforce.h
@@ -1,4 +1,4 @@
 #pragma once
 
-void bruteForceMatMult(const float *A, const float *B, float *C, int n,
-                       int k, int m);
+void GEMM(const float *A, const float *B, float *C, float alpha, float beta,
+          int n, int k, int m);
